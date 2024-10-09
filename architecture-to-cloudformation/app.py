02d491aa57ec5a676bb32e1f5aff41cc0281c5d2
sbm1977 <sbmcculloch@gmail.com>
2024-10-09 09:02:46 +0100

    Add files via upload

--- architecture-to-cloudformation/app.py
+++ architecture-to-cloudformation/app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 import util
 from argparse import ArgumentParser
 
@@ -8,27 +7,31 @@ parser.add_argument("--modelId", type=str, default=None)
 
 args = parser.parse_args()
 st.set_page_config(
-    page_title="AWS",
-    page_icon="👋",
+    page_title="Cloud Infrastructure",
+    page_icon="☁️",
     layout="wide",
 )
 
 modelId = args.modelId
 
-st.header("Architecture to CloudFormation")
+st.header("Architecture to Infrastructure as Code")
 
 # Using object notation
 
 st.sidebar.header("Inference Parameters")
-Temperature = st.sidebar.slider(
-    "Temperature", min_value=0.0, max_value=1.0, step=0.1, value=0.0
-)
+Temperature = st.sidebar.slider("Temperature", min_value=0.0, max_value=1.0, step=0.1, value=0.0)
 Top_P = st.sidebar.slider("Top P", min_value=0.0, max_value=1.0, step=0.001, value=1.0)
 Top_K = st.sidebar.slider("Top K", min_value=0, max_value=500, step=1, value=250)
+
+# Add radio buttons for cloud provider and infrastructure as code tool
+cloud_provider = st.sidebar.radio("Cloud Provider", ["AWS", "Azure"])
+iac_tool = st.sidebar.radio("Infrastructure as Code Tool", ["CloudFormation", "Terraform"])
 
 bedrock = util.Model(
     modelId=modelId,
     inference_params={"temperature": Temperature, "top_p": Top_P, "top_k": Top_K},
+    cloud_provider=cloud_provider,
+    iac_tool=iac_tool,
 )
 
 if st.button("Clear", type="secondary"):
@@ -38,20 +41,18 @@ if st.button("Clear", type="secondary"):
     st.rerun()
 
 uploaded_file = st.file_uploader(
-    "Upload an Architecture diagram to generate AWS CloudFormation code",
+    f"Upload an Architecture diagram to generate {cloud_provider} {iac_tool} code",
     type=["jpeg", "png"],
     disabled=bool(bedrock.get_explain()),
 )
 
 if uploaded_file is not None:
-
     image, explain = st.columns((5, 5))
 
     with image:
         st.image(uploaded_file.getvalue())
 
     with explain:
-
         if bedrock.get_explain():
             st.write(bedrock.get_explain())
         else:
@@ -70,16 +71,12 @@ if uploaded_file is not None:
             with st.chat_message(role):
                 st.markdown(content)
 
-
     if not bedrock.check_memory():
         with st.chat_message("assistant"):
             code_placeholder = st.empty()
-
             bedrock.invoke_code_model(code_placeholder)
 
-    if prompt := st.chat_input(
-        "Give the bot instructions to update stack...",
-    ):
+    if prompt := st.chat_input("Give the bot instructions to update the infrastructure..."):
         with st.chat_message("human"):
             st.markdown(prompt)
 
